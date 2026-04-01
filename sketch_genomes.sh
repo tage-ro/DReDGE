e896267f9ed7c77f2ec4821d6c53ccbc4086e682
tage-ro <rosenqvist.tage@gmail.com>
2026-04-01 14:00:53 +0200

    Create sketch_genomes.sh

---
+++ sketch_genomes.sh
@@ -0,0 +1,8 @@
+# USAGE: sketch_genomes.sh <dereplicated genomes directory> <output name> <compression> <cores>
+
+GENOMES=$1
+OUTPUT=$2
+COMPRESS=$3
+CORES=$4
+
+sylph sketch ${GENOMES}/*.fna -c $COMPRESS -o $OUTPUT -t $CORES
