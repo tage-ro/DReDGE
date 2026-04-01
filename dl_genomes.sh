b441f556002f38695e4c0a5b0032663946f3ceaa
tage-ro <rosenqvist.tage@gmail.com>
2026-04-01 13:52:27 +0200

    Create dl_genomes.sh

---
+++ dl_genomes.sh
@@ -0,0 +1,8 @@
+# USAGE: dl_genomes.sh <taxid> <output directory>
+
+TAXON=$1
+OUT=$2
+
+datasets download genome taxon $TAXON --reference --dehydrated --filename genomes.zip
+unzip genomes.zip -d $OUT
+datasets rehydrate --directory $OUT/
